99a24acc51ae657ccb4ecfb073a673f672b08d3d
MirceaAurelian <danaurelianmircea@yahoo.com>
2024-01-15 23:46:14 +0200

    Fix movement facing mouse direction

--- Assets/src/Movements.cs
+++ Assets/src/Movements.cs
@@ -9,33 +9,31 @@ public class Movements : MonoBehaviour
     public float jumpAmount = 10;
     private bool isGrounded = true;
     private Vector3 mousePosition;
-
     private float angleInRadians;
 
     private void Update()
     {
+        this.angleInRadians = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+        // this.angleInRadians = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            angleInRadians = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
-            Vector3 movement = new Vector3(Mathf.Cos(angleInRadians), 0f, Mathf.Sin(angleInRadians)) * speed * Time.deltaTime;
-
-            transform.position += movement;
-            transform.position += new Vector3(speed * Time.deltaTime + Convert.ToSingle(Math.Cos(Convert.ToDouble(transform.rotation.eulerAngles.y))), 0f, Convert.ToSingle(Math.Cos(Convert.ToDouble(transform.rotation.eulerAngles.y))));
+            transform.position += new Vector3(Mathf.Cos(this.angleInRadians), 0f, -Mathf.Sin(this.angleInRadians)) * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(speed * Time.deltaTime + Convert.ToSingle(Math.Cos(Convert.ToDouble(transform.rotation.eulerAngles.y))), 0f, Convert.ToSingle(Math.Cos(Convert.ToDouble(transform.rotation.eulerAngles.y))));
+            transform.position -= new Vector3(Mathf.Cos(this.angleInRadians), 0f, -Mathf.Sin(this.angleInRadians)) * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
+            //transform.position += new Vector3(Mathf.Cos(transform.rotation.eulerAngles.y) * Mathf.Sin(transform.rotation.eulerAngles.y), 0f, 1f) * speed * Time.deltaTime;
+            transform.position += new Vector3(Mathf.Sin(this.angleInRadians), 0f, Mathf.Cos(this.angleInRadians)) * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            transform.position -= new Vector3(0f, 0f, speed * Time.deltaTime);
+            transform.position -= new Vector3(Mathf.Sin(this.angleInRadians), 0f, Mathf.Cos(this.angleInRadians)) * speed * Time.deltaTime;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
