93b7bd6a094cf2e58affd74dff38322854256527
Andrei-Cristian Criste <criste.andrei30@yahoo.com>
2024-05-13 23:50:51 +0300

    Fix bad positioning for client

--- Assets/src/NetworkPlayer.cs
+++ Assets/src/NetworkPlayer.cs
@@ -12,26 +12,36 @@ public class NetworkPlayer : NetworkBehaviour
     private Button button;
     public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
 
+    // TO DO:
+    /*
+        La fiecare client nou conectat, pe ServerRpc pune camera player-ului pe disabled
+        La fiecare client nou conectat, parcruge toti ceilalti clienti (inclusiv serverul) si pune prin ClientRpc camera lor pe disabled
+    */
     public override void OnNetworkSpawn()
     {
-        Move();
-    }
-
-    public void Move()
-    {
-        SubmitPositionRequestServerRpc();
+        inputField.text = OwnerClientId.ToString();
+        transform.name = "Player" + OwnerClientId.ToString();
+        transform.position = new Vector3(-4581f, 9890.5f, -169f);
+        if (NetworkManager.Singleton.ConnectedClients.Count > 1) {
+            HideCameraForServerRpc(OwnerClientId);
+            HideCameraForClientRpc(OwnerClientId);
+        }
     }
 
     [ServerRpc]
-    void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
+    void HideCameraForServerRpc(ulong id, ServerRpcParams rpcParams = default)
     {
-        transform.position = new Vector3(-4581f ,9890.5f ,-169f);
-        Position.Value = transform.position;
+        NetworkManager.Singleton.ConnectedClients[id].PlayerObject.transform.Find("Camera").gameObject.SetActive(false);
     }
 
-    void Update()
+    [ClientRpc]
+    void HideCameraForClientRpc(ulong id, ClientRpcParams rpcParams = default)
     {
-        Position.Value = transform.position;
+        foreach(var connectedClient in NetworkManager.Singleton.ConnectedClients) {
+            if (connectedClient.Key != id) {
+                connectedClient.Value.PlayerObject.transform.Find("Camera").gameObject.SetActive(false);
+            }
+        }
     }
 
     public void UpdateButtonClick(Action f) {
