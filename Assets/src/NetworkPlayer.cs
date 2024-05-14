f71e1e5057b496d24bfc859e3173dedda68802db
Andrei-Cristian Criste <criste.andrei30@yahoo.com>
2024-05-14 23:59:00 +0300

    Add basic Multiplayer

--- Assets/src/NetworkPlayer.cs
+++ Assets/src/NetworkPlayer.cs
@@ -1,58 +1,61 @@
-using Unity.Netcode;
+﻿using Unity.Netcode;
 using UnityEngine;
 using TMPro;
-using System;
 using UnityEngine.UI;
+using System;
 
 public class NetworkPlayer : NetworkBehaviour
 {
     [SerializeField]
+    private GameObject playerCamera; 
+    [SerializeField]
     private TMP_InputField inputField;
     [SerializeField]
     private Button button;
-    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
 
-    // TO DO:
-    /*
-        La fiecare client nou conectat, pe ServerRpc pune camera player-ului pe disabled
-        La fiecare client nou conectat, parcruge toti ceilalti clienti (inclusiv serverul) si pune prin ClientRpc camera lor pe disabled
-    */
+    void Start()
+    {
+        InitializeCamera();
+    }
+
+    void InitializeCamera()
+    {
+        if (IsLocalPlayer)
+        {
+            playerCamera.SetActive(true);
+        }
+        else
+        {
+            playerCamera.SetActive(false);
+        }
+    }
+
     public override void OnNetworkSpawn()
     {
-        inputField.text = OwnerClientId.ToString();
-        transform.name = "Player" + OwnerClientId.ToString();
-        transform.position = new Vector3(-4581f, 9890.5f, -169f);
-        if (NetworkManager.Singleton.ConnectedClients.Count > 1) {
-            HideCameraForServerRpc(OwnerClientId);
-            HideCameraForClientRpc(OwnerClientId);
-        }
+            inputField.text = OwnerClientId.ToString();
+            transform.name = "Player" + OwnerClientId.ToString();
+            transform.position = new Vector3(-4581f, 9890.5f, -169f);
     }
 
-    [ServerRpc]
-    void HideCameraForServerRpc(ulong id, ServerRpcParams rpcParams = default)
+    public void UpdateButtonClick(Action f)
     {
-        NetworkManager.Singleton.ConnectedClients[id].PlayerObject.transform.Find("Camera").gameObject.SetActive(false);
-    }
-
-    [ClientRpc]
-    void HideCameraForClientRpc(ulong id, ClientRpcParams rpcParams = default)
-    {
-        foreach(var connectedClient in NetworkManager.Singleton.ConnectedClients) {
-            if (connectedClient.Key != id) {
-                connectedClient.Value.PlayerObject.transform.Find("Camera").gameObject.SetActive(false);
-            }
-        }
-    }
-
-    public void UpdateButtonClick(Action f) {
         button.onClick.AddListener(() => { Debug.LogWarning("Button clicked"); f.Invoke(); });
     }
 
-    public void ShowIP(ushort ip) {
-        inputField.text = ip.ToString();
+    public void ShowIP(ushort ip)
+    {
+        if (IsLocalPlayer)
+        {
+            inputField.text = ip.ToString();
+        }
     }
 
-    public ushort GetIP() {
-        return System.Convert.ToUInt16(inputField.text);
+    public ushort GetIP()
+    {
+        if (IsLocalPlayer)
+        {
+            return Convert.ToUInt16(inputField.text);
+        }
+        return 0;
     }
 }
